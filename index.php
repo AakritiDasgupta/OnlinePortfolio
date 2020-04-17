0d7a13df4008b01d3f989356578e1b945453bc2d
AakritiDasgupta <57162376+AakritiDasgupta@users.noreply.github.com>
2020-04-16 17:08:42 -0700

    Added all files on repl.it portfolio

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Aakriti Dasgupta  & Period 1]</p>'; ?> 
+    
+  <ul class="nav nav-pills nav-fill">
+    <li class="nav-item">
+      <a class="nav-link active" href="index.html">Index.html</a>
+    </li>
+    <li class="nav-item">
+     <a class="nav-link" href="Dasgupta_AboutMe.html">AboutMe.html
+     </a>
+    </li>
+    <li> 
+      <a class="nav-link" href="Dasgupta_Projects.html">Projects.html
+      </a>
+    </li>
+    <li >
+       <a class="nav-link" href="Dasgupta_WorkExperience.html">Work Experience.html
+       </a>
+    </li>
+    <li>
+        <a class="nav-link" href= "Dasgupta_ContactMe.html" tabindex="-1" aria-disabled="true">ContactMe.html
+       </a>
+    </li>
+  </ul >
+  </body>
+</html>
